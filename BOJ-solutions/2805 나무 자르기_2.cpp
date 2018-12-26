520af3857dd10057d55a64d5678a0681ee9e534b
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2018-12-26 11:21:22 +0900

    이분 탐색

---
+++ BOJ-solutions/2805 나무 자르기_2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int N;
+    int M;
+    int tree[1000010];
+    cin>>N>>M;
+    
+    for(int i=0;i<N;i++)
+        cin>>tree[i];
+    
+    int minn = 0;
+    int maxx = 1000000000;
+    
+    while(minn+1 < maxx)
+    {
+        int midd = (minn+maxx) /2;
+        long long int sum = 0;
+        for(int i=0;i<N;i++)
+        {
+            if(tree[i] > midd)
+                sum += tree[i] - midd ;
+        }
+        
+        if(sum>=M)
+            minn = midd;
+        else
+            maxx = midd;
+    }
+    
+    cout<<minn<<endl;
+    
+}
