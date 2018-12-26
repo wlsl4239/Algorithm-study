3498fd2950014d81389a858b7a78510fb6ff6450
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2018-12-26 11:25:04 +0900

    수학

---
+++ BOJ-solutions/3046 R2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int a;
+    int avg;
+    
+    cin>>a>>avg;
+    
+
+    int b = 2*avg - a;
+    
+    cout<<b<<endl;
+    
+    return 0;
+    
+}
