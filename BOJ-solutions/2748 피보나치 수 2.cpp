f3b71ea792a9d492ce8d5595b25aedc79908255e
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2018-12-26 11:20:51 +0900

    DP

---
+++ BOJ-solutions/2748 피보나치 수 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long int arr[3];
+    
+    int a;
+    
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 1;
+    
+    cin>>a;
+    
+    for(int i=2;i<a;i++)
+    {
+        arr[0] = arr[1];
+        arr[1] = arr[2];
+        arr[2] = arr[0]+arr[1];
+    }
+    
+    cout<<arr[2]<<endl;
+}
