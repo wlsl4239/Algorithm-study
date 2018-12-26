9504f09481c979cb431d53d5b6e4d243d0f43574
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2018-12-26 12:01:01 +0900

    문자열 처리

---
+++ BOJ-solutions/11365 !밀비 급일.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+
+using namespace std;
+
+
+int main()
+{
+	string s;
+
+	while (1)
+	{
+		getline(cin, s);
+		
+		if (s == "END")
+			break;
+
+		for (int i = s.size() - 1;i >= 0;i--)
+			cout << s[i];
+		cout << endl;
+	}
+}
