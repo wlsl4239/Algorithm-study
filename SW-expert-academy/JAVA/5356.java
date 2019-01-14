79332dd717dd6d039a6cff6244dba3048d27508e
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2019-01-14 13:28:20 +0900

    Add files via upload

---
+++ SW-expert-academy/JAVA/5356.java
@@ -0,0 +1,39 @@
+package array2;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Solution5356_의석이의세로로말해요_서울9반_김경수2 {
+	public static void main(String args[]) throws Exception	{
+		System.setIn(new FileInputStream("res/input5356.txt"));
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			StringBuffer ans = new StringBuffer("");
+			String arr[] = new String[5];
+			int len = 0;
+			
+			for(int i=0;i<5;i++) {
+				arr[i] = sc.next();
+				len = (len<arr[i].length())?arr[i].length():len;
+			}
+				
+			for(int i=0;i<len;i++) {
+				for(int j=0;j<5;j++) {
+					if(arr[j].length() <= i)
+						continue;
+					ans.append(arr[j].charAt(i));
+				}
+			}
+			
+
+			System.out.println("#" + test_case + " " + ans);
+		}
+
+	}
+
+}
