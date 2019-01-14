37904d60db476e2bae5ef50128d6696b9185dda3
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2019-01-14 10:39:21 +0900

    Add files via upload

---
+++ SW-expert-academy/JAVA/2817.java
@@ -0,0 +1,42 @@
+package array2;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Solution2817_부분수열의합_서울9반_김경수 {
+	public static void main(String args[]) throws Exception	{
+		System.setIn(new FileInputStream("res/input2817.txt"));
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int ans = 0;
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			int arr[] =	new int [n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+			
+			for(int i=1;i<(1<<n);i++) {
+				int sum = 0;
+				
+				for(int j=0;j<n;j++) {
+					if(((1<<j) & i) != 0) {
+						sum += arr[j];
+					}
+				}
+				
+				if(sum==k) {
+					ans ++;
+				}	
+			}
+			
+			System.out.println("#" + test_case + " " + ans);
+		}
+
+	}
+
+}
