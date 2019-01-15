4853c00c4239d93a47cee6a9184e33b5eeed1d11
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2019-01-15 13:21:51 +0900

    Update 1213.java

--- SW-expert-academy/JAVA/1213.java
+++ SW-expert-academy/JAVA/1213.java
@@ -9,21 +9,14 @@ public class Solution {
 		for(int test_case = 1; test_case <= 10; test_case++)
 		{
 			T=sc.nextInt();
-			
 			String query = sc.next();
 			String text = sc.next();
+			
 			int ans = 0;
-			for(int i=0;i<=text.length() - query.length();i++) {
-				boolean flag = true;
-				for(int j=0;j<query.length();j++) {
-					char a = text.charAt(i+j);
-					char b = query.charAt(j);
-					if(a != b)
-						flag = false;						
-				}
-				if(flag)
+			for(int i=0;i<=text.length() - query.length();i++) 
+				if(text.substring(i,i+query.length()).equals(query))
 					ans++;
-			}
+			
 			System.out.println("#"+test_case+" " + ans);
 		}
 	}
