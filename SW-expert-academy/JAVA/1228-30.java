73a60fbc386dae1a1f59fa490b5956fe6623e8d0
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2019-01-28 16:53:06 +0900

    Update 1228-30.java

--- SW-expert-academy/JAVA/1228-30.java
+++ SW-expert-academy/JAVA/1228-30.java
@@ -1,10 +1,8 @@
-//1228,1229,1230 암호문 1~3 
-
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Solution {
-	public static void main(String[] args) throws Exception {
+public class Solution{
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		for(int test = 1;test<=10;test++) {
@@ -19,35 +17,34 @@ public class Solution {
 			n = sc.nextInt();
 			for(int i=0;i<n;i++) {
 				String s = sc.next();
-				
+
+				int x,y;
 				switch(s) {
 				case "I":
-					int num = sc.nextInt();
-					int k = sc.nextInt();
-					for(int j=0;j<k;j++) {
-						int newnum = sc.nextInt();
-						ll.add(num, newnum);
-						num++;
+					x = sc.nextInt();
+					y = sc.nextInt();
+					for(int j=0;j<y;j++) {
+						ll.add(x, sc.nextInt());
+						x++;
 					}
 					break;
 					
 				case "D":
-					int num1 = sc.nextInt();
-					int k1 = sc.nextInt();
-					for(int j=0;j<k1;j++) {;
-						ll.remove(num1);
-					}
+					x = sc.nextInt();
+					y = sc.nextInt();
+					for(int j=0;j<y;j++) 
+						ll.remove(x);
 					break;
 					
 				case "A":
-					int k2 = sc.nextInt();
-					for(int j=0;j<k2;j++) {
-						int newnum = sc.nextInt();
-						ll.addLast(newnum);
-					}
+					y = sc.nextInt();
+					for(int j=0;j<y;j++) 
+						ll.addLast(sc.nextInt());
 					break;
 				}
-			}			
+				
+			}
+			
 			System.out.print("#" + test + " ");
 			for(int i=0;i<10;i++) {
 				System.out.print(ll.removeFirst() + " ");
