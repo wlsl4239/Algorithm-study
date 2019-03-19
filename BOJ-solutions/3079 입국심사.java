c288bc269da9b8f852a9d5566721d5d2a838e8e5
wlsl4239 <32384397+wlsl4239@users.noreply.github.com>
2019-03-19 11:45:23 +0900

    Create 3079 입국심사.java

---
+++ BOJ-solutions/3079 입국심사.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String[] s = br.readLine().split(" ");
+			int n = Integer.parseInt(s[0]);
+			int m = Integer.parseInt(s[1]);
+			long maxtime = 0;
+			int[] time = new int[n];
+			for(int i=0;i<n;i++) {
+				time[i] = Integer.parseInt(br.readLine().trim());
+				maxtime = Math.max(maxtime, time[i]);
+			}
+
+			long time1 = 0;
+			long time2 = maxtime * m;
+			long ans = maxtime * m;
+			long mid;
+
+			while (time1 <= time2){
+				mid = (time1 + time2) / 2;
+				long answer = 0;
+
+				for (int i = 0; i < n; i++)
+					answer += (mid / time[i]);
+
+				if (answer < m)
+					time1 = mid + 1;
+
+				else {
+					ans = Math.min(ans, mid);
+					time2 = mid - 1;
+				}
+			}
+			System.out.println(ans);
+		
+	}
+}
